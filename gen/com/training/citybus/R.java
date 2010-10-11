ec5bcb339f4c9cc3671aca432249186894280bdf
MinoSius <misterminos@gmail.com>
2010-10-11 19:41:38 +0300

    second lesson - menus

--- gen/com/training/citybus/R.java
+++ gen/com/training/citybus/R.java
@@ -11,16 +11,23 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int ic_menu_exit=0x7f020000;
+        public static final int icon=0x7f020001;
     }
     public static final class id {
-        public static final int mapview=0x7f050000;
+        public static final int mapview=0x7f060000;
+        public static final int quit=0x7f060001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
+    public static final class menu {
+        public static final int context_menu=0x7f050000;
+        public static final int icon_menu=0x7f050001;
+    }
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int quit=0x7f040002;
     }
 }
--- src/com/training/citybus/CityBus.java
+++ src/com/training/citybus/CityBus.java
@@ -1,6 +1,12 @@
 package com.training.citybus;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
 
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
@@ -16,7 +22,39 @@ public class CityBus extends MapActivity {
     }
     
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.icon_menu, menu);
+        return true;
+    }
+    
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenuInfo menuInfo) {
+      super.onCreateContextMenu(menu, v, menuInfo);
+      MenuInflater inflater = getMenuInflater();
+      inflater.inflate(R.menu.context_menu, menu);
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+        case R.id.quit:
+            quit();
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+    }
+    
+    @Override
     protected boolean isRouteDisplayed() {
         return false;
     }
+    
+    private void quit() {
+    	finish();
+    }
+    
 }
